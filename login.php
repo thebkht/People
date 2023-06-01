03f8dc0b82eccf49e80e8a96525e8ba6bb4f1bb7
Bakhtiyor Ganijon <b.yusupoff001@gmail.com>
2023-06-02 04:56:54 +0900

    Updated login form

--- login.php
+++ login.php
@@ -2,40 +2,35 @@
 // Include the necessary database connection code
 require_once "db_connection.php";
 
-// Initialize the error variable
-$error = "";
+// Check if the user is already logged in
+session_start();
+if (isset($_SESSION["user_id"])) {
+    header("Location: index.php");
+    exit();
+}
 
 // Process the login form submission
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    $email = $_POST["email"];
+    $username = $_POST["username"];
     $password = $_POST["password"];
 
-    // Validate user input if needed
-
-    // Check if the email exists in the database
-    $stmt = $conn->prepare("SELECT * FROM users WHERE email = ?");
-    $stmt->bind_param("s", $email);
+    // Check if the username exists in the database
+    $stmt = $conn->prepare("SELECT * FROM users WHERE username = ?");
+    $stmt->bind_param("s", $username);
     $stmt->execute();
     $result = $stmt->get_result();
     $user = $result->fetch_assoc();
     $stmt->close();
 
-    if ($user) {
-        // Verify the password
-        if (password_verify($password, $user["password"])) {
-            // Password is correct, start the session and store user data if needed
-            session_start();
-            $_SESSION["user_id"] = $user["user_id"];
-            $_SESSION["username"] = $user["username"];
+    if ($user && password_verify($password, $user["password"])) {
+        // Store the user ID in the session
+        $_SESSION["user_id"] = $user["user_id"];
 
-            // Redirect the user to the dashboard or any other desired page
-            header("Location: index.php");
-            exit();
-        } else {
-            $error = "Invalid password.";
-        }
+        // Redirect the user to the homepage
+        header("Location: index.php");
+        exit();
     } else {
-        $error = "Email not found.";
+        $error_message = "Invalid username or password.";
     }
 }
 ?>
@@ -45,28 +40,32 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 <head>
     <meta charset="UTF-8">
     <title>Login</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
     <div class="container">
         <h1>Login</h1>
-        <?php if ($error): ?>
-            <div class="alert alert-danger"><?php echo $error; ?></div>
+
+        <?php if (isset($error_message)): ?>
+            <div class="alert alert-danger" role="alert">
+                <?php echo $error_message; ?>
+            </div>
         <?php endif; ?>
+
         <form action="<?php echo $_SERVER["PHP_SELF"]; ?>" method="POST">
             <div class="mb-3">
-                <label for="email" class="form-label">Email</label>
-                <input type="email" name="email" id="email" class="form-control" required>
+                <label for="username" class="form-label">Username</label>
+                <input type="text" class="form-control" id="username" name="username" required>
             </div>
             <div class="mb-3">
                 <label for="password" class="form-label">Password</label>
-                <input type="password" name="password" id="password" class="form-control" required>
+                <input type="password" class="form-control" id="password" name="password" required>
             </div>
-            <div class="mb-3">
-                <button type="submit" class="btn btn-primary">Login</button>
-            </div>
+            <button type="submit" class="btn btn-primary">Login</button>
         </form>
-        <p>Don't have an account? <a href="signup.php">Sign up</a></p>
     </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-gPcuybB0syW5DjCZVDCDj4FCOBubVUoTLV9iDCDXjzfOiR7SLw6eSKbWJ4EODCK6" crossorigin="anonymous"></script>
 </body>
 </html>
