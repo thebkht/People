74ce2f8a00f38cc2d6e14866f43953264dbc4fb8
Bakhtiyor Ganijon <62228656+yusupovbg@users.noreply.github.com>
2023-06-12 22:26:13 +0900

    Update index.php

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
 <html>
 <head>
     <meta charset="UTF-8">
-    <title>Readit</title>
+    <title>people</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
